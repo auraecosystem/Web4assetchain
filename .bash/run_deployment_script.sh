1644f6f633cf02c4bbe0fbc31840a0d0c7056ff6
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-15 10:20:57 +0100

    Update run_deployment_script.sh

--- .bash/run_deployment_script.sh
+++ .bash/run_deployment_script.sh
@@ -1,1 +1,3 @@
-npx hardhat run scripts/deploy.js --network <web4asset>
+npx hardhat run scripts/deploy.js --fadaka-blockchain <web4asset>
+
+npx hardhat run scripts/deploy.js --web4-blockchain <web4asset>
