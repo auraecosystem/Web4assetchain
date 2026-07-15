e874b87960e24c787471885f6765a3d59a3b8972
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-15 10:06:11 +0100

    Update Node.sh

--- .bash/Node.sh
+++ .bash/Node.sh
@@ -1,6 +1,6 @@
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
+git submodule update --init --recursive
 # Install Node.js and npm
 sudo apt install nodejs npm
 
@@ -13,10 +13,21 @@ cd web4asset-node
 
 # Compile the Node
 cargo build --release
-
+git submodule update --init --recursive
 mkdir docs
 cp -r build/* docs/
-
+docker compose down -v && docker compose up --build -d
 truffle compile
 truffle migrate --network development
 npm install --save-dev webpack-dev-server
+sudo certbot --nginx -d {your_domain_or_subdomain}
+sudo bash -c "$(curl -fsSL https://dv.net/install.sh)"
+git clone --recursive https://github.com/dv-net/dv-bundle.git
+cd dv-bundle
+cp .env.example .env  # Configure environment variables if necessary
+docker compose up -d
+# Update all submodules to the latest versions
+git submodule update --remote
+
+# Rebuild and restart services
+docker compose up --build -d
